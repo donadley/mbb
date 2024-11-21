ed3d4283ad103d9e316080480637b5fec77276f8
Don <>
2024-11-21 09:32:11 -0600

    added nojekyll to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import { resolve } from 'path'; // Import resolve to specify file paths
+import { copyFileSync } from 'fs'; // Import to handle copying files
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -26,7 +28,15 @@ export default defineConfig({
       manifest: false,
       devOptions: {
         enabled: true
+      }    
+    }),
+    {
+      name: 'add-nojekyll', // Name the custom plugin
+      apply: 'build', // Ensure it runs during the build
+      closeBundle() {
+        // Copy the .nojekyll file to the `dist` directory
+        copyFileSync(resolve(__dirname, '.nojekyll'), resolve(__dirname, 'dist', '.nojekyll'));
       }
-    })
+    }
   ]
 })
